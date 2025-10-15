db0c926134da9492e70dea8b74aa798ee4aa3327
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-10-15 13:23:30 +0600

    redux installed successfully

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,24 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from './Button';
 
-const Counter = () => {
-    const [count, setCount] = useState(0);
-    const handleIncrement = () => {
-        setCount((prv) => prv + 1);
-    };
-    const handleDecrement = () => {
-        if(count === 0){
-            setCount(0);
-        }else{
-            setCount((prv) => prv - 1);
-        }
-    };
+const Counter = ({ count, onIncrement, onDecrement }) => {
     return (
-        <div className='p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow'>
+        <div className='p-4 h-40 flex flex-col items-center justify-center space-y-5 bg-white rounded shadow'>
             <h2 className='text-2xl font-semibold'>{count}</h2>
             <div className='flex space-x-3'>
-                <Button handler={handleIncrement}>Increment</Button>
-                <Button handler={handleDecrement} type="danger">Decrement</Button>
+                <Button handler={() => onIncrement()}>Increment</Button>
+                <Button handler={onDecrement} type="danger">Decrement</Button>
             </div>
         </div>
     );
--- src/Components/HomeLayout.jsx
+++ src/Components/HomeLayout.jsx
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './Counter';
+import Stats from './Stats';
+
+const initialCounters = [
+    {
+        id: 1,
+        value: 0
+    },
+    {
+        id: 2,
+        value: 0
+    },
+]
 
 const HomeLayout = () => {
+    const [counters, setCounters] = useState(initialCounters);
+
+    const handleIncrement = (counterId) => {
+        const updatedCounters = counters.map(counter => {
+            if(counter.id === counterId){
+                return {
+                    ...counter,
+                    value: counter.value + 1
+                };
+            }
+
+            return counter
+        });
+
+        setCounters(updatedCounters)
+    };
+
+    const handleDecrement = (counterId) => {
+        const updatedCounters = counters.map(counter => {
+            if(counter.value === 0){
+                return counter
+            }
+            if(counter.id === counterId){
+                return {
+                    ...counter,
+                    value: counter.value - 1
+                };
+            }
+
+            return counter
+        });
+
+        setCounters(updatedCounters)
+    };
+
+    const totalCount = counters.reduce( (sum, current) => sum + current.value, 0 )
+
     return (
         <div className='w-screen h-screen p-10 bg-gray-100 text-slate-700'>
             <h1 className='max-w-md mx-auto text-center text-2xl font-bold'>Simple Counter Application</h1>
             <div className='max-w-md mx-auto mt-10 space-y-5'>
-                <Counter/>
+                {
+                    counters.map(counter => 
+                        <Counter 
+                            key={counter.id}
+                            count={counter.value} 
+                            onIncrement={() => handleIncrement(counter.id)}
+                            onDecrement={() => handleDecrement(counter.id)}
+                            />
+                    )
+                }
+                <Stats totalCount={totalCount}/>
             </div>
         </div>
     );
